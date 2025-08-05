1eefa1910cb782fa30f1812d173bde46cada8971
Formularios UTCD <sayoxlive@gmail.com>
2025-08-05 12:02:23 -0600

    Update GuardarAnti.js

--- functions/api/GuardarAnti.js
+++ functions/api/GuardarAnti.js
@@ -1,6 +1,6 @@
 // POST  /api/GuardarAnti  →  Proxy a Apps Script (registrar o actualizar)
 const GAS_URL =
-  "https://script.google.com/macros/s/AKfycbx3IIT0YBtLAbLzrcNMrfqTRHvMJ5mWFh1YulE8NrVCypT-6kPcl-VristYMGdRO1kz/exec";
+  "https://script.google.com/macros/s/AKfycbzrnZAcxALQRl5rUh1OQLKoHoSarEzX8EUODyfUk2K6MDG-tb9Uf4FgJXeAvFKrsGkb/exec;
 
 export async function onRequestPost({ request }) {
   const origin = request.headers.get("Origin") || "*";
