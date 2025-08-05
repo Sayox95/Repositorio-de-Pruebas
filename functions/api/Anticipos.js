98ae03129fbf5b6748bfff88804cb06310fa46f5
Formularios UTCD <sayoxlive@gmail.com>
2025-08-05 11:16:48 -0600

    Update Anticipos.js

--- functions/api/Anticipos.js
+++ functions/api/Anticipos.js
@@ -1,6 +1,6 @@
 // GET  /api/Anticipos  →  Proxy a Apps Script ?accion=leerAnticipos
 const GAS_URL =
-"https://script.google.com/macros/s/AKfycbwkeitkaLq-XIapwYrirjY6OfSaUnUatOWApm8Gof4cK8mE1KaJAtxiqdCpoYumXwfd/exec";
+"https://script.google.com/macros/s/AKfycbzxlPdLxw5GJPsogsu9je02eqNxtuPAK82aacos396nJG_RLsOg5McIXLvfjO7kjoaW/exec";
 
 export async function onRequestGet({ request }) {
   const origin = request.headers.get("Origin") || "*";
