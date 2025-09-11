a40e197e3e2008892dfc29553217e470b340dc9e
Formularios UTCD <sayoxlive@gmail.com>
2025-09-11 11:43:18 -0600

    Create logout.js

---
+++ functions/api/logout.js
@@ -0,0 +1,8 @@
+export async function onRequest() {
+  return new Response(JSON.stringify({ok:true}), {
+    headers: {
+      "Content-Type":"application/json",
+      "Set-Cookie": "utcd_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
+    }
+  });
+}
