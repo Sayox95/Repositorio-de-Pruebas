f008dd5da26dab1bd03975254f0d3504e143e757
Formularios UTCD <sayoxlive@gmail.com>
2025-09-11 13:08:39 -0600

    Update create.js

--- functions/api/auth/create.js
+++ functions/api/auth/create.js
@@ -22,7 +22,7 @@ export async function onRequest({ request, env }) {
 
   // Usa variable de entorno si existe; si no, usamos tu URL
   const GAS_URL = env.GAS_AUTH_URL
-    ?? "https://script.google.com/macros/s/AKfycbyYZ-B8M5Xftd3NSXtszVhfmoV-mihNEVpGL1I_8VbjVT-z3yJsf8WGXL_iUcXzUpfm/exec";
+    ?? "https://script.google.com/macros/s/AKfycbz_fjwblx09ywK-y-6RiSIOB88LUdVVsy_r8fR8_18l7Llky6hczAgBXy6cp7eKLl8r/exec";
 
   // Llamada a GAS
   const res = await fetch(GAS_URL, {
