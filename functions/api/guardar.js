9df9d1e6817098f9be0dbdb669b7c943843aacfe
Formularios UTCD <sayoxlive@gmail.com>
2025-10-09 15:15:14 -0600

    Update guardar.js

--- functions/api/guardar.js
+++ functions/api/guardar.js
@@ -30,7 +30,7 @@ export async function onRequestPost({ request }) {
   let resp;
   try {
     resp = await fetch(
-     "https://script.google.com/macros/s/AKfycbw-SLgFU7IFpHK9ug6q1GbyTaIXAwOmlCHETxVXG38JdnNF8KiXcUxJ4CnZrbyShzVNig/exec, 
+     "https://script.google.com/macros/s/AKfycbw-SLgFU7IFpHK9ug6q1GbyTaIXAwOmlCHETxVXG38JdnNF8KiXcUxJ4CnZrbyShzVNig/exec", 
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
