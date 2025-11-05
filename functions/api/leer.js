c627621cfea7a3fa1396ce58b9c2ded51a0e10f4
Formularios UTCD <sayoxlive@gmail.com>
2025-11-05 16:57:30 -0600

    Update leer.js

--- functions/api/leer.js
+++ functions/api/leer.js
@@ -6,7 +6,7 @@ export async function onRequestGet({ request }) {
   const estados     = incoming.searchParams.get("estados");     // opcional
   const ids         = incoming.searchParams.getAll("ids");      // múltiples ids para byId
 
-  const url = new URL("https://script.google.com/macros/s/AKfycbz-QMvr8tL0LTvFM6g5g2z6vsA8VRusOnUPpurmeacPZli9XUoNJQJWswDkBRpoY0DfUQ/exec"); 
+  const url = new URL("https://script.google.com/macros/s/AKfycbxXX33uWG9XFaF1Hh8UvrEBxMOSsDN2B2OYicpQE4kVGRidwJLCJuZJ3otmqkR4p_nAEQ/exec"); 
 
   if (otrosCargos) {
     // Modo "Otros Cargos"
